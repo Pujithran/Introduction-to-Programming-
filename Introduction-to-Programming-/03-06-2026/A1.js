a73c29daa5381255b9caf011ddd9ef976bef24c9
Pujithran <pujithran.uki@dreamspace.academy>
2026-06-03 12:40:13 +0530

    feat: Added a score calculator

--- Introduction-to-Programming-/03-06-2026/A1.js
+++ Introduction-to-Programming-/03-06-2026/A1.js
@@ -44,3 +44,53 @@ function MultiplesOfFive(num){
 }
 
 MultiplesOfFive(20)
+
+
+
+function CelsiusToFahrenheit(Cel){
+    Celsius = Cel
+    Fahrenheit = 0
+
+    Fahrenheit = (Celsius*9/5) + 32
+
+    console.log(Celsius+"°C is equal to "+Fahrenheit+"°F")
+}
+    
+CelsiusToFahrenheit(36)
+
+
+function Grade(marks){
+    score = marks
+    result = 0
+
+    if(score > 100){
+        console.log("Invalid Score")
+    }
+
+    else if(score > 80){
+        console.log("A grade")
+    }
+
+    else if(score > 70){
+        console.log("B grade")
+    }
+
+    else if(score > 60){
+        console.log("C grade")
+    }
+
+    else if(score > 50){
+        console.log("D grade")
+    }
+
+    else if(score > 0){
+        console.log("F grade")
+    }
+
+    else{
+        console.log("Invalid Score")
+    }
+
+}
+
+Grade(82)
