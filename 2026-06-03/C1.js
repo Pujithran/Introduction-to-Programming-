10ae9c66921338bb408e55cb93600af96ab0bf86
Pujithran <pujithran.uki@dreamspace.academy>
2026-06-04 09:45:40 +0530

    feat:Made some minor changes in calculations

--- 2026-06-03/C1.js
+++ 2026-06-03/C1.js
@@ -7,6 +7,8 @@ function bill(unit){
     rate=0
 
     total=0
+
+    temp=0
     
     if (unit > 0 && unit < 60){
         
@@ -16,6 +18,7 @@ function bill(unit){
         }
         
         else{
+            temp=unit-30
             fixed=210
             rate=9
         }
@@ -30,31 +33,35 @@ function bill(unit){
         }
 
         else if (unit < 90){
+            temp=unit-60
             fixed=400
             rate=20
         }
 
         else if (unit < 120){
+            temp=unit-90
             fixed=1000
             rate=28
         }
 
         else if (unit < 180){
+            temp=unit-120
             fixed=1500
             rate=44
         }
 
         else {
+            temp=unit-180
             fixed=2100
             rate=85
         }
     
     }
 
-    total = rate*unit + fixed
+    total = (rate*temp) + fixed
 
     console.log("Your total bill is "+total+".Rs")
 
 }
 
-bill(280)
+bill(300)
